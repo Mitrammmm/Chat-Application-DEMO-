44ef33a5cf8fef6d7a898e5a4027d4508a709ed8
Saumitra singh <139237985+Mitrammmm@users.noreply.github.com>
2025-03-27 19:32:38 +0530

    Update ChatRoom.jsx

--- frontend/src/ChatRoom.jsx
+++ frontend/src/ChatRoom.jsx
@@ -2,7 +2,9 @@ import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+// const socket = io("http://localhost:5000");
+const socket = io("https://chat-application-demo-u36k.onrender.com");
+
 
 function ChatRoom() {
     const { room, name } = useParams();
