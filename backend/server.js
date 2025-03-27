5269467d28ca7cf02a66d757ebf11b693d460b08
Saumitra singh <139237985+Mitrammmm@users.noreply.github.com>
2025-03-27 19:43:52 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173", // Adjust based on frontend port
+        origin: ["http://localhost:5173", "https://chat-karo-qlpy.onrender.com"], // Adjust based on frontend port
         methods: ["GET", "POST"]
     }
 });
